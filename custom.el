933b32534a12b6ab69433a780a1e7ea53ed6b85b
Garrett Rowe <garrett.m.rowe@gmail.com>
2026-04-19 22:21:58 -0700

    Add corfu to custom.el

--- custom.el
+++ custom.el
@@ -10,19 +10,25 @@
      "9655ef5a1221c33b53d9cbdd1a04090ca8a458015a1dd365344e1bb3c3cfcf81"
      "1d51919e9a075a4f56396681dc99adecefa152ab29819178403cbf6ae5bc0dd8"
      default))
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(ace-window cider clang-format-lite company corfu flycheck-clj-kondo
+                flycheck-rust ivy lsp-mode move-text neotree
+                org-superstar paredit parinfer-rust-mode projectile
+                rustic simplicity-theme smex which-key zprint-mode))
  '(safe-local-variable-directories
    '("/Users/garrettrowe/dev/graph/" "/Users/garrettrowe/dev/anim8/"))
  '(safe-local-variable-values
    '((eval setq-local company-clang-arguments
-	   (list
-	    (concat "-I"
-		    (expand-file-name "raylib/src"
-				      (projectile-project-root)))))))
+           (list
+            (concat "-I"
+                    (expand-file-name "raylib/src"
+                                      (projectile-project-root)))))))
  '(warning-suppress-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background nil)))))
+ '(default ((t (:background nil))))
+ '(font-lock-keyword-face ((t (:foreground "#fe8019"))))
+ '(font-lock-type-face ((t (:foreground "#b8bb26")))))
